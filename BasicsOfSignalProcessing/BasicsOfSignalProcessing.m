1e0a8246441268b168221c3c2a373e58184826f4
Erfan Rasti <erfanrasty@gmail.com>
2023-09-24 10:00:44 +0330

    Add pre requirements and clear the workspace

---
+++ BasicsOfSignalProcessing/BasicsOfSignalProcessing.m
@@ -0,0 +1,13 @@
+%% Basics of Signal Processing in Time and frequency domains
+%% Pre requirements
+% * Matlab R2018b or later
+% * Signal Processing Toolbox
+% * Filter Designer Toolbox
+% * Visual Studio Code (optional)
+% * Matlab official release extension for Visual Studio Code (optional)
+% * Git (optional)
+
+%% Clear the workspace
+close all;
+clear;
+clc;
