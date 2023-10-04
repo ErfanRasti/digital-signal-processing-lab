05211249f089b93cea91424866f669724ba6a836
Erfan Rasti <erfanrasty@gmail.com>
2023-10-04 08:54:13 +0330

    Add dirac comb function

--- BasicsOfSignalProcessing/BasicSignals.m
+++ BasicsOfSignalProcessing/BasicSignals.m
@@ -22,6 +22,9 @@ clc;
 % single sample in discrete-time signal processing.
 % * The dirac delta function is used to represent the impulse response of
 % an LTI system.
+% * The infinity can not be represented in MATLAB. Therefore, we can
+% represent the dirac delta function as $1/T_s = f_s$, where $T_s$ is the
+% sampling time and $f_s$ is the sampling frequency.
 % We can define continuous-time dirac delta function as follows:
 fs = 1e4;
 t = -10:1 / fs:10;
@@ -46,4 +49,37 @@ stem(n, delta, 'LineWidth', 1.5);
 xlabel('Time (s)');
 ylabel('Amplitude');
 title('Discrete-Time Dirac Delta Function');
+grid on;
+
+%% Dirac Comb Function
+% The Dirac comb function is a periodic function that is zero everywhere except
+% at integer multiples of the period, where it is infinite.
+% * The Dirac comb function is used to represent the sampling process in
+% signal processing.
+% * The Dirac comb function is used to represent the periodic impulse
+% response of an LTI system.
+% We can define continuous-time Dirac comb function as follows:
+fs = 1e4;
+t = -10:1 / fs:10;
+N = length(t);
+figure('Name', 'Dirac Comb Function');
+dirac_comb = zeros(N, 1);
+dirac_comb(mod(t, 1) == 0) = fs;
+plot(t, dirac_comb, 'LineWidth', 1.5);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Continuous-Time Dirac Comb Function');
+grid on;
+
+%%%
+% We can define discrete-time Dirac comb function as follows:
+n = -10:10;
+N = length(n);
+figure('Name', 'Discrete-Time Dirac Comb Function');
+dirac_comb = zeros(N, 1);
+dirac_comb(mod(n, 1) == 0) = 1;
+stem(n, dirac_comb, 'LineWidth', 1.5);
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('Discrete-Time Dirac Comb Function');
 grid on;
