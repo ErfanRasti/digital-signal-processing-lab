b2191443fd84af91498685f2849f8bcce4c69896
Erfan Rasti <erfanrasty@gmail.com>
2023-10-11 06:34:28 +0330

    Add explanations of FS of discrete-time signal

--- BasicsOfSignalProcessing/FourierSeriesAndTransforms.m
+++ BasicsOfSignalProcessing/FourierSeriesAndTransforms.m
@@ -8,10 +8,10 @@ clc;
 % In MATLAB, both time and frequency axes are discrete. So we cannot use
 % transforms which are defined for continuous time or frequency.
 % The only operation that can be performed discretely in both time and
-% frequency domains is Fourier coefficients of discrete time periodic signals.
-% The more general form of Fourier series for discrete time periodic signals
+% frequency domains is Fourier coefficients of discrete-time periodic signals.
+% The more general form of Fourier series for discrete-time periodic signals
 % is called Discrete Fourier Transform (DFT).
-% The DFT of a discrete time signal $x[n]$ is defined as
+% The DFT of a discrete-time signal $x[n]$ is defined as
 % $$X[k] = \sum_{n=0}^{N-1} x[n] e^{-j\frac{2\pi}{N}kn}$$
 % where $N$ is the period of the signal.
 %
@@ -33,3 +33,24 @@ clc;
 % The DFT and inverse DFT commands can be used without specifying the length
 % of the vector. In this case, the length of the vector is assumed to be the
 % length of the vector given as input.
+%
+% Other Fourier operations in MATLAB should be implemented by using the DFT
+% and inverse DFT commands. The FFT algorithm is used to compute the DFT.
+% The FFT algorithm is a fast algorithm to compute the DFT.
+
+%% Fourier Series of a discrete-time periodic signal
+% The Fourier series of a discrete-time periodic signal $x[n]$ is defined as
+% $$x[n] = \sum_{k=0}^{N-1} X[k] e^{j\frac{2\pi}{N}kn}$$
+% where $N$ is the period of the signal.
+%
+% The inverse Fourier series is defined as
+% $$X[k] = \frac{1}{N} \sum_{n=0}^{N-1} x[n] e^{-j\frac{2\pi}{N}kn}$$
+%
+% The Fourier series of a discrete-time periodic signal is the DFT of the
+% signal. So, the Fourier series of a discrete-time periodic signal can be
+% computed by the command |fft|.
+% The inverse Fourier series is the inverse DFT of the signal. So, the
+% inverse Fourier series of a discrete-time periodic signal can be computed
+% by the command |ifft|.
+% The following example shows the Fourier series of a discrete-time periodic
+% signal.
