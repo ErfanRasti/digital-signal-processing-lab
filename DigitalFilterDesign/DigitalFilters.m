a2e817bd959c4d7a499ec0e0d3f1bb7fb78048e1
Erfan Rasti <erfanrasty@gmail.com>
2023-10-18 11:30:21 +0330

    Add FIR filter explanations

--- DigitalFilterDesign/DigitalFilters.m
+++ DigitalFilterDesign/DigitalFilters.m
@@ -3,3 +3,9 @@
 close all;
 clear;
 clc;
+
+%% Design FIR filters in the time domain
+% # We can define differnet filters in time domain with finite impulse response (FIR).
+% # The filter coefficients are the impulse response of the filter.
+% # We cannot design IIR filters in time domain; because the impulse response of IIR filters are infinite.
+
