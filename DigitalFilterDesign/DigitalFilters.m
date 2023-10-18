3023d4243095c4df5c6317640b373fbf93f8eeeb
Erfan Rasti <erfanrasty@gmail.com>
2023-10-18 11:30:42 +0330

    Add Hamming window explanations

--- DigitalFilterDesign/DigitalFilters.m
+++ DigitalFilterDesign/DigitalFilters.m
@@ -9,3 +9,10 @@ clc;
 % # The filter coefficients are the impulse response of the filter.
 % # We cannot design IIR filters in time domain; because the impulse response of IIR filters are infinite.
 
+%% Hamming window
+% # The Hamming window is a taper formed by using a weighted cosine.
+% # It was first proposed by Richard Hamming in 1950, when he was an employee of Bell Labs.
+% # It is used to reduce the noise in the signal.
+% # The Hamming window is defined as:
+%
+% $$ w(n) = \alpha - \beta \cos \left( \frac{2 \pi n}{N-1} \right) $$
