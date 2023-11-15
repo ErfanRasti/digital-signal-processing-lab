b7abf8a5f3da81b2cd0ce7aa33dcacac5d66bb3a
Erfan Rasti <erfanrasty@gmail.com>
2023-11-15 06:47:37 +0330

    Add ZTransform.m

---
+++ DigitalAudioSignalProcessing/ZTransform.m
@@ -0,0 +1,18 @@
+%% * Z-trnasform *
+%% Clear the workspace
+close al;
+clear;
+clc;
+%% Why Z-transform?
+%
+% # The Z-transform is a powerful tool for analyzing discrete-time systems.
+% # It is the discrete-time counterpart of the Laplace transform, which is
+% used to analyze continuous-time systems.
+% # The Z-transform is useful in the design and analysis of discrete-time
+% systems.
+% # It provides a convenient representation of the input-output behavior
+% of discrete-time systems.
+% # The Z-transform is also useful in solving difference equations.
+% The Z-transform is a generalization of the DTFT.
+% # The DTFT is the Z-transform evaluated on the unit circle.
+%
