2029f179cf4d537ec11491cab4b1b3254c87af3c
Erfan Rasti <erfanrasty@gmail.com>
2023-12-06 11:48:50 +0330

    Add image reading functionality

--- DigitalImageProcessing/ImageAnalyzing.m
+++ DigitalImageProcessing/ImageAnalyzing.m
@@ -3,3 +3,10 @@
 close all;
 clear;
 clc;
+
+%% Read the image
+% The first step to analyze an image is to read it.
+% The |imread| function reads the image from the file specified by the
+% string filename, inferring the format of the file from its contents.
+% |imread| supports a variety of graphics file formats and returns the
+% image.
