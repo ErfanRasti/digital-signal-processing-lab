f2af449fc8b1260a41d05fb1c9b9ed52f94b00f1
Erfan Rasti <erfanrasty@gmail.com>
2023-12-06 12:21:14 +0330

    Add code to read and display an image

--- DigitalImageProcessing/ImageAnalyzing.m
+++ DigitalImageProcessing/ImageAnalyzing.m
@@ -10,3 +10,11 @@ clc;
 % string filename, inferring the format of the file from its contents.
 % |imread| supports a variety of graphics file formats and returns the
 % image.
+img = imread('coins.png');
+
+%% Display the image
+% The |imshow| function displays the image in the current axes. The image
+% is displayed at the resolution of the screen. The image is automatically
+% scaled to fit the screen if its size is larger than the screen.
+figure('Name', 'Original Image');
+imshow(img);
