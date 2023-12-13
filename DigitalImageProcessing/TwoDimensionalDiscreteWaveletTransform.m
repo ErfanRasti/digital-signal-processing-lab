731baa898f23d4d71f3b92b7251249849ab3950f
Erfan Rasti <erfanrasty@gmail.com>
2023-12-13 11:21:47 +0330

    Add 2-D DWT algorithm explanation

--- DigitalImageProcessing/TwoDimensionalDiscreteWaveletTransform.m
+++ DigitalImageProcessing/TwoDimensionalDiscreteWaveletTransform.m
@@ -26,3 +26,41 @@ clc;
 % where $\mathbf{H}$ is the 2-D DWT matrix, $\mathbf{X}$ is the image
 % matrix, and $\mathbf{W}$ is the 2-D DWT of the image matrix.
 %
+%% 2-D DWT Algorithm
+% The 2-D DWT algorithm is as follows:
+%
+% # Apply the 1-D DWT to the rows of the image.
+% # Apply the 1-D DWT to the columns of the resulting images
+%
+% The Algorithm is shown in the following figure:
+%
+imshow('images/2d_dwt_algorithm.png');
+%%%
+% # Apply low pass filter and high pass filter to the rows of the image and
+% create two branches of the image.
+% # Downsample the two branches of the image by a factor of 2 across the
+% columns(keep only the even columns).
+% # Apply low pass filter and high pass filter to the columns of the image
+% and create four branches of the image.
+% # Downsample the four branches of the image by a factor of 2 across the
+% rows(keep only the even rows).
+%
+% According to the above algorithm, number of pixels of each of the
+% coefficients of final decomposed image is quarter of the original image.
+% So the total number of pixels of all coefficients is equal to the number of
+% pixels of the original image.
+%
+% The final output consists of four subbands: LL, LH, HL, and HH.
+%
+% * The LL subband is the approximation coefficients of the original image.
+% * The LH subband is the horizontal detail coefficients of the original
+% image.
+% * The HL subband is the vertical detail coefficients of the original
+% image.
+% * The HH subband is the diagonal detail coefficients of the original
+% image.
+%
+% LH subband can be used to reconstruct the horizontal edges of the image.
+% HL subband can be used to reconstruct the vertical edges of the image.
+%
+
